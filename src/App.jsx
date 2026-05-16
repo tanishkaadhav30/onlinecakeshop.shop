d95ac5fb5c900350eb2a2fa688809c5d3a245116
tanishkaadhav30 <tanishkaadhav30@gmail.com>
2026-05-16 18:29:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1> welcome to the cake shop </h1>
+      <p> Enjoy our delicious cakes! </p>
+         </div>
+  )
+}
+
+export default App
