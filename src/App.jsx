fb6e92365a97507159fc4024fcd926c08e87e9f5
tanishkaadhav30 <tanishkaadhav30@gmail.com>
2026-05-16 20:03:32 +0530

    yml file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
@@ -13,6 +5,5 @@ function App() {
       <p> Enjoy our delicious cakes! </p>
          </div>
   )
-}
 
 export default App
